65ce7175c7c8d368a27db629a7f47fb74491fef6
Maarten <maartenwpost@gmail.com>
2017-09-08 00:04:10 +0200

    update

--- got-risk/src/app/app.module.ts
+++ got-risk/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PlayersComponent } from './components/players/players.component';
+import { AddPlayerComponent } from './components/add-player/add-player.component';
+import { PlayerDetailsComponent } from './components/player-details/player-details.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { HousesComponent } from './components/houses/houses.component';
+
+const appRoutes: Routes = [
+  {path:'', component:DashboardComponent},
+  {path:'login', component:LoginComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    PlayersComponent,
+    AddPlayerComponent,
+    PlayerDetailsComponent,
+    NavComponent,
+    LeaderboardComponent,
+    LoginComponent,
+    HousesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
